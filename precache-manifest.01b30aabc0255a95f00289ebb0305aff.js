31ea3d8a68b49fe7b5293bb152962a72f7b64118
Akimi Yano <akimi.mimi.yano@gmail.com>
2020-04-20 23:29:01 -0700

    Updates

---
+++ precache-manifest.01b30aabc0255a95f00289ebb0305aff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63e77a126b8921197fece6708c1376cb",
+    "url": "/coronavirus-prediction/index.html"
+  },
+  {
+    "revision": "78ebcde4feb12a7b954d",
+    "url": "/coronavirus-prediction/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2d4159eda66da2a5398a",
+    "url": "/coronavirus-prediction/static/js/2.47f38820.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coronavirus-prediction/static/js/2.47f38820.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78ebcde4feb12a7b954d",
+    "url": "/coronavirus-prediction/static/js/main.c2023ff2.chunk.js"
+  },
+  {
+    "revision": "dcaba553442583f0e9f0",
+    "url": "/coronavirus-prediction/static/js/runtime-main.992535c3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/coronavirus-prediction/static/media/logo.5d5d9eef.svg"
+  }
+]);
